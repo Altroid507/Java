31e34bb1a2ee80e5d0a462a0ac00d3ebdf7f5f79
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2024-01-24 16:32:37 +0300

    Add files via upload

--- Drobn.java
+++ Drobn.java
@@ -12,12 +12,25 @@ public class Drobn {
             System.out.println("Знаменатель не может быть равен нулю.");
             return;
         }
+        if (ch < 0 && zn < 0){
+            ch *= -1;
+            zn *= -1;
+        }
+        else if (zn < 0){
+            ch *= -1;
+            zn *= -1;
+        }
         this.ch = ch;
         this.zn = zn;
     }
 
     private static int gcd(int a, int b){
-        int c;
+        int c = 1;
+        if (a > b){
+            int maxim = a;
+            a = b;
+            b = maxim;
+        }
         while (a % b != 0){
             c = a % b;
             a = b;
@@ -26,9 +39,33 @@ public class Drobn {
         return c;
     }
     private void razion(){
-        int gcd1 = gcd(ch, zn);
+        int gcd1 = gcd(zn, ch);
+        if (gcd1 < 0){
+            gcd1 *= -1;
+        }
         ch = ch / gcd1;
         zn = zn / gcd1;
+    }
+
+    private int get_ch(){
+        this.razion();
+        return this.ch;
+    }
+
+    private int get_zn(){
+        this.razion();
+        return this.zn;
+    }
+
+    private boolean pravilno(){
+        if (this.ch >= this.zn){
+            return false;
+        }
+        return true;
+    }
+
+    public String toString(){
+        return (this.ch + "/" + this.zn);
     }
 
     public static void main(String[] args) {
@@ -36,7 +73,10 @@ public class Drobn {
         int a = in.nextInt();
         int b = in.nextInt();
         Drobn d1 = new Drobn(a, b);
-        d1.razion();
-        System.out.println(d1.ch + " " + d1.zn);
+        System.out.println(d1.toString());
+        //System.out.println(d1.pravilno());
+        //d1.razion();
+        //System.out.println(d1.get_ch() + " " + d1.get_zn());
+        //System.out.println(d1.ch + " " + d1.zn);
     }
 }
